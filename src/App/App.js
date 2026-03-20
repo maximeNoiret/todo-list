744737f01ef2832d68876765d5cf886720b13ff9
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2026-03-20 09:16:53 +0100

    uh: uh...? What even is react where's my good old C.......

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import logo from './logo.svg'
+import Header from '../Header';
+//import ToDo from '../ToDo';
+//import Footer from '../Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header logo={logo} />      
+    </div>
+  );
+}
+
+export default App;
